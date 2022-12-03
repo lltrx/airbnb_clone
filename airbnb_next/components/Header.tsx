6d9cf6e446471ba55f960b4db57404b2e826060f
@2614579a <2614579a@student.gla.ac.uk>
2022-12-03 18:48:30 +0300

    Done with the header and the banner

--- airbnb_next/components/Header.tsx
+++ airbnb_next/components/Header.tsx
@@ -1,5 +1,11 @@
 import Image from "next/image";
-import { SearchIcon } from "@heroicons/react/solid";
+import { 
+  SearchIcon,
+  GlobeAltIcon,
+  MenuIcon,
+  UserCircleIcon
+
+} from "@heroicons/react/solid";
 
 function Header() {
   return (
@@ -22,7 +28,15 @@ function Header() {
        rounded-full p-2 cursor-pointer md:mx-2" />
      </div>
      {/* right section */}
-     <div></div>
+     <div className="flex items-center space-x-4 justify-end text-gray-500">
+        <p className="hidden md:inline cursor-pointer">Become a host</p>
+        <GlobeAltIcon className="h-6 cursor-pointer "/>
+
+        <div className="flex items-center space-x-2 border-2 p-2 rounded-full ">
+          <MenuIcon className="h-6"/>
+          <UserCircleIcon className="h-6"/>
+        </div>
+     </div>
 
     </header> 
   )
