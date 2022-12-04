9b4851d1934c9459b62f7792a91f0f3fd2a35eea
@2614579a <2614579a@student.gla.ac.uk>
2022-12-04 08:39:17 +0300

    added the smallcard in the main, fetch the data using server side

--- airbnb_next/pages/index.tsx
+++ airbnb_next/pages/index.tsx
@@ -1,10 +1,10 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from "../components/Header"
 import Banner from "../components/Banner"
+import SmallCard from "../components/utils/smallCard"
 
-const Home: NextPage = () => {
+export default function Home({exploreData}) {
   return (
     <div className="">
       <Head>
@@ -16,8 +16,40 @@ const Home: NextPage = () => {
     <Header />
     {/* Banner */}
     <Banner />
+    {/* Main */}
+    <main className="max-w-7xl mx-auto px-8 sm:px-16">
+      <section className="pt-6">
+        <h2 className="text-4xl font-semibold pb-5">
+          Explore Nearby 
+        </h2>
+        {/* Pull some data from a server - API endpoints */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {exploreData?.map(({img, distance, location}) => (
+          <SmallCard
+          key={img}
+          img={img}
+          distance={distance}
+          location={location}
+           />
+        ))}
+        </div>
+      </section>
+    </main>
     </div>
   )
 }
 
-export default Home
+export async function getStaticProps(){
+  const exploreData = await fetch('https://www.jsonkeeper.com/b/4G1G')
+  .then(
+    (res) => res.json()
+  )
+
+  return {
+    props: {
+      exploreData,
+
+  }
+}
+
+}
