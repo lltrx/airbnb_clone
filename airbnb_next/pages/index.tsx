8cd1c62c66dda12b84a89dc034f1f7388955e0cf
@2614579a <2614579a@student.gla.ac.uk>
2022-12-06 15:05:05 +0300

    Done with the frontend, change some little things

--- airbnb_next/pages/index.tsx
+++ airbnb_next/pages/index.tsx
@@ -3,8 +3,11 @@ import Image from 'next/image'
 import Header from "../components/Header"
 import Banner from "../components/Banner"
 import SmallCard from "../components/utils/smallCard"
+import MediumCard from "../components/utils/mediumCard"
+import LargeCard from "../components/utils/largeCard"
+import Footer from "../components/Footer"
 
-export default function Home({exploreData}) {
+export default function Home({exploreData, cardsData}) {
   return (
     <div className="">
       <Head>
@@ -34,7 +37,30 @@ export default function Home({exploreData}) {
         ))}
         </div>
       </section>
+
+      <section>
+        <h2 className="text-4xl font-semibold py-8">
+          Live Anywhere
+        </h2>
+        <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3">
+        {cardsData?.map(({img,title}) =>
+          <MediumCard
+          key={img}
+          img={img}
+          title={title}
+          />
+          )}
+        </div>
+      </section>
+
+      <LargeCard
+      img="https://links.papareact.com/4cj"
+      title="The Greatest Outdoors"
+      description="Wishlists curated by Airbnb."
+      buttonText="Get Inspired"
+      />
     </main>
+    <Footer />
     </div>
   )
 }
@@ -45,10 +71,15 @@ export async function getStaticProps(){
     (res) => res.json()
   )
 
+  const cardsData = await fetch('https://www.jsonkeeper.com/b/VHHT')
+  .then(
+    (res) => res.json()
+  )
+
   return {
     props: {
       exploreData,
-
+      cardsData
   }
 }
 
