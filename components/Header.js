d94424d34390ccde56e8ac5587e41f30c5564e99
@2614579a <2614579a@student.gla.ac.uk>
2022-12-07 23:38:21 +0300

    Finished the first part of the serach function

--- components/Header.js
+++ components/Header.js
@@ -11,12 +11,14 @@ import { useState } from "react";
 import "react-date-range/dist/styles.css"; // main style file
 import "react-date-range/dist/theme/default.css"; // theme css file
 import { DateRangePicker } from "react-date-range";
+import { useRouter } from "next/dist/client/router";
 
-function Header() {
+function Header({placeholder}) {
   const [searchInput, setSearchInput ] = useState("");
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
   const [noOfGuests, setNoOfGuests] = useState(1);
+  const router = useRouter();
 
   const selectionRange ={
     startDate: startDate,
@@ -34,17 +36,26 @@ function Header() {
   }
 
   const search = () => {
-    console.log("search");
+    router.push({
+      pathname: "/search",
+      query: {
+        location: searchInput,
+        startDate: startDate.toISOString(),
+        endDate: endDate.toISOString(),
+        noOfGuests,
+      },
+    });
   }
-
-  
-
-
 
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md py-5 px-5 md:px-10 ">
+
      {/* left section */}
-    <div className="relative flex items-center h-10 cursor-pointer my-auto">
+
+    <div 
+    onClick={() => router.push("/")}
+    className="relative flex items-center h-10 cursor-pointer my-auto">
+
       <Image
         src="https://links.papareact.com/qd3"
         alt="airbnb logo"
@@ -52,15 +63,21 @@ function Header() {
         objectFit="contain"
         objectPosition="left"
         />
+
     </div>
+
      {/* center section */}
+
      <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
+
       <input 
       value={searchInput}
       onChange={(e) => setSearchInput(e.target.value)}
       className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400" 
       type="text" 
-      placeholder="Start your search" />
+      placeholder={placeholder || "Start your search"}
+      />
+
       <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white
        rounded-full p-2 cursor-pointer md:mx-2" />
      </div>
@@ -85,8 +102,11 @@ function Header() {
         />
 
         <div className="flex items-center border-b mb-4">
+
           <h2 className="text-2xl flex-grow font-semibold">Number of Guests</h2>
+
           <UsersIcon className="h-5" />
+
           <input
             value={noOfGuests}
             onChange={(e) => setNoOfGuests(e.target.value)}
@@ -96,6 +116,7 @@ function Header() {
           />
 
         </div>
+
         <div className="flex">
 
           <button className="flex-grow text-gray-500" onClick={resetInput}>
@@ -108,7 +129,7 @@ function Header() {
 
         </div>
       </div>
-    )
+    ) 
 
     }
     </header> 
