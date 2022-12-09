3a691dd380734c52c88d26fcdfcbe7a0e9a17d9b
@2614579a <2614579a@student.gla.ac.uk>
2022-12-09 06:19:27 +0300

    Done with the project, ready to deploy

--- components/utils/map.js
+++ components/utils/map.js
@@ -1,27 +1,71 @@
-import Map from 'react-map-gl'
+import ReactMapGL from 'react-map-gl';
+import { Marker, Popup } from 'react-map-gl';
 import { useState } from 'react';
+import getCenter from 'geolib/es/getCenter';
+function Map({searchResults}) {
 
-function map() {
-     
-     const [viewport, setViewport] = useState({
-          width: "100%",
-          height: "100%",
-          latitude: 37.7577,
-          longitude: -122.4376,
-          zoom: 11,
-     });
+   const [selectedLocation, setSelectedLocation] = useState({});
+
+   const coordinates = searchResults.map(result => ({
+      longitude: result.long,
+      latitude: result.lat,
+   }));
+   
+   const center = getCenter(coordinates);
+   
+   const [viewport, setViewport] = useState({
+      width: "100%",
+      height: "100%",
+      latitude: center.latitude,
+      longitude: center.longitude,
+      zoom: 11,
+   });
 
   return (
-     <Map
-     mapStyle="mapbox://styles/turxv/clbf4gj6r004114qf14ikqwcm"
-     mapboxApiAccessToken={process.env.mapbox_key}
-     {...viewport}
-     onViewportChange={(nextViewport) => setViewport(nextViewport)}
+     <ReactMapGL
+      mapStyle="mapbox://styles/turxv/clbf4gj6r004114qf14ikqwcm"
+      mapboxApiAccessToken={process.env.mapbox_key}
+      {...viewport}
+      onViewportChange={(nextViewport) => setViewport(nextViewport)}
      >
-     </Map>
+       {searchResults.map(result => (
+            <div key={result.long}>
+               <Marker
+               longitude={result.long}
+               latitude={result.lat}
+               offsetLeft={-20}
+               offsetTop={-10}
+               >
+                  <p
+                  role="img"
+                  onClick={() => setSelectedLocation(result)}
+                  className="cursor-pointer text-2xl animate-bounce"
+                  aria-label="push-pin"
+                  >📌</p>
+
+               </Marker>
+
+               {selectedLocation.long === result.long ? (
+                  <Popup
+                  onClose={() => setSelectedLocation({})}
+                  closeOnClick={true}
+                  latitude={result.lat}
+                  longitude={result.long}
+                  >
+                     {result.title}
+                  </Popup>
+               ) : (
+                  false
+               )}
+               
+            </div>
+         ))} 
+
+
+     </ReactMapGL>
 
      
   )
 }
 
-export default map
+export default Map
